ade12b114122da9c351083764ee258aea5f18a3c
Ali Raza <alirazaqasim192@gmail.com>
2024-11-18 18:30:47 +0500

    useContext hook introduction

---
+++ 08-mini-context-vite-app/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react"
+import UserContext from "../context/UserContext"
+
+function Profile() {
+
+  const {user} = useContext(UserContext)
+
+  if (!user) return <h1>Please Login</h1>
+
+  return (
+    <div>
+      
+
+      <h1>Profile Name is: {user.username}</h1>
+    </div>
+  )
+}
+
+export default Profile
