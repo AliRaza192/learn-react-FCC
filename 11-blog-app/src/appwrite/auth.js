02bb4dcdefc70bc17eefad62e7b2a1689d4369ca
Ali Raza <alirazaqasim192@gmail.com>
2024-11-20 00:19:34 +0500

    mega project auth section almost done

---
+++ 11-blog-app/src/appwrite/auth.js
@@ -0,0 +1,62 @@
+import Conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(Conf.appwriteURL)
+      .setProject(Conf.appwriteProjectID);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSessionSession(
+        email,
+        password
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUsers() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite Service :: getCurrentUsers() :: ", error);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite Service :: logout() :: ", error);
+      
+    }
+  }
+}
+
+const authService = new AuthService()
+export default authService
---
+++ 11-blog-app/src/conf/conf.js
@@ -0,0 +1,10 @@
+const Conf = {
+    appwriteURL: String(process.env.APP_WRITE_URL),
+    appwriteProjectID: String(process.env.APP_WRITE_PROJECT_ID),
+    appwriteDatabaseID: String(process.env.APP_WRITE_DATABASE_ID),
+    appwriteCollectionID: String(process.env.APP_WRITE_COLLECTION_ID),
+    appwriteBucketID: String(process.env.APP_WRITE_BUCKET_ID)
+
+}
+
+export default Conf
