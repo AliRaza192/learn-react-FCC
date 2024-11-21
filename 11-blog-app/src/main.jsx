ab1ced9fe76dada6c2eb09ab359875a905c0fb68
Ali Raza <alirazaqasim192@gmail.com>
2024-11-21 12:08:13 +0500

    react hook form

--- 11-blog-app/src/main.jsx
+++ 11-blog-app/src/main.jsx
@@ -1,10 +1,84 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import AddPost from "./pages/AddPost.jsx";
+import EditPost from "./pages/EditPost.jsx";
+import Post from "./pages/Post.jsx";
+import AllPosts from "./pages/AllPosts.jsx";
+import store from "./store/store.js";
+import Protected from "./components/AuthLayout.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <Protected authentication={false}>
+            <Login />
+          </Protected>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <Protected authentication={false}>
+            <Signup />
+          </Protected>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <Protected authentication={true}>
+            <AllPosts />
+          </Protected>
+        ),
+      },
+      {
+        path: "/add-posts",
+        element: (
+          <Protected authentication={true}>
+            <AddPost />
+          </Protected>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <Protected authentication={true}>
+            <EditPost />
+          </Protected>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: (
+          <Protected authentication={true}>
+            <Post />
+          </Protected>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
